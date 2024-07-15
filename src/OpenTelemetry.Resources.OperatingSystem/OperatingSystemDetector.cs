3696072b8929d645e8df3fdfa71cb017847dd376
James Thompson <thompson.tomo@outlook.com>
2024-07-16 08:59:01 +1000

    #1957 Provide additional OS fields

--- src/OpenTelemetry.Resources.OperatingSystem/OperatingSystemDetector.cs
+++ src/OpenTelemetry.Resources.OperatingSystem/OperatingSystemDetector.cs
@@ -16,21 +16,68 @@ internal sealed class OperatingSystemDetector : IResourceDetector
     /// <returns>Resource with key-value pairs of resource attributes.</returns>
     public Resource Detect()
     {
+
+        var attributes = new List<KeyValuePair<string, object>>()
+            {
+            new(AttributeOperatingSystemBuild, Environment.OSVersion.Version.Build),
+            new(AttributeOperatingSystemDescription, Environment.OSVersion.VersionString),
+            new(AttributeOperatingSystemVersion, Environment.OSVersion.Version.ToString()),
+            };
         var osType = GetOSType();
 
-        if (osType == null)
+        if (osType != null)
         {
-            return Resource.Empty;
+            attributes.Add(new KeyValuePair<string, object>(AttributeOperatingSystemType, osType));
         }
+        var osName = GetOSName();
 
-        return new Resource(
-        [
-            new(AttributeOperatingSystemType, osType),
-        ]);
+        return new Resource(attributes);
     }
 
     private static string? GetOSType()
     {
+#if NET5_0_OR_GREATER
+        if (System.OperatingSystem.IsAndroid())
+        {
+            return OperatingSystemsValues.Android;
+        }
+        if (System.OperatingSystem.IsBrowser())
+        {
+            return OperatingSystemsValues.Android;
+        }
+        if (System.OperatingSystem.IsFreeBSD())
+        {
+            return OperatingSystemsValues.FreeBSD;
+        }
+        if (System.OperatingSystem.IsIOS())
+        {
+            return OperatingSystemsValues.IOS;
+        }
+        if (System.OperatingSystem.IsLinux())
+        {
+            return OperatingSystemsValues.Linux;
+        }
+        if (System.OperatingSystem.IsMacCatalyst())
+        {
+            return OperatingSystemsValues.MacCatalyst;
+        }
+        if (System.OperatingSystem.IsMacOS())
+        {
+            return OperatingSystemsValues.MacOS;
+        }
+        if (System.OperatingSystem.IsTvOS())
+        {
+            return OperatingSystemsValues.TvOS;
+        }
+        if (System.OperatingSystem.IsWatchOS())
+        {
+            return OperatingSystemsValues.WatchOS;
+        }
+        if (System.OperatingSystem.IsWindows())
+        {
+            return OperatingSystemsValues.Windows;
+        }
+#else
         var platform = Environment.OSVersion.Platform;
         if (platform == PlatformID.Win32NT)
         {
@@ -39,14 +86,14 @@ internal sealed class OperatingSystemDetector : IResourceDetector
 
         if (platform == PlatformID.MacOSX)
         {
-            return OperatingSystemsValues.Darwin;
+            return OperatingSystemsValues.MacOS;
         }
 
         if (platform == PlatformID.Unix)
         {
             return OperatingSystemsValues.Linux;
         }
-
+#endif
         return null;
     }
 }
--- src/OpenTelemetry.Resources.OperatingSystem/OperatingSystemSemanticConventions.cs
+++ src/OpenTelemetry.Resources.OperatingSystem/OperatingSystemSemanticConventions.cs
@@ -6,6 +6,10 @@ namespace OpenTelemetry.Resources.OperatingSystem;
 internal static class OperatingSystemSemanticConventions
 {
     public const string AttributeOperatingSystemType = "os.type";
+    public const string AttributeOperatingSystemName= "os.name";
+    public const string AttributeOperatingSystemVersion = "os.version";
+    public const string AttributeOperatingSystemDescription = "os.description";
+    public const string AttributeOperatingSystemBuild = "os.build_id";
 
     public static class OperatingSystemsValues
     {
@@ -14,5 +18,33 @@ internal static class OperatingSystemSemanticConventions
         public const string Linux = "linux";
 
         public const string Darwin = "darwin";
+
+        public const string FreeBSD = "freebsd";
+
+        public const string NetBSD = "netbsd";
+
+        public const string OpenBSD = "openbsd";
+
+        public const string DragonFlyBSD = "dragonflybsd";
+
+        public const string HPUX = "hpux";
+
+        public const string AIX = "aix";
+
+        public const string Solaris = "solaris";
+
+        public const string ZOS = "z_os";
+
+        public const string Android = "android";
+
+        public const string IOS = "ios";
+
+        public const string MacCatalyst = "maccatalyst";
+
+        public const string MacOS = "macosx";
+
+        public const string TvOS = "tvos";
+
+        public const string WatchOS = "watchos";
     }
 }
